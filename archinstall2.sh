fb427f5c78990ef262fd11cb3791f309bdaf28b6
ajjames31 <ajjames31@gmail.com>
2016-06-22 11:42:46 -0500

    split the script into 2

--- archinstall2.sh
+++ archinstall2.sh
@@ -1,58 +1,9 @@
-#!/bin/bash
+#this is the second part of the install script, after you have chroot into the new intall
 
+echo "=========================="
+echo "Arch Install Script"
+echo "=========================="
 
-#This is a script to install Arch Linux
-clear
-echo "===================================="
-echo "Arch Linux Install Script"
-echo "===================================="
-echo
-#list drives
-lsblk -S
-
-#Choose drive
-echo "Enter the drive that you want to use in /dev/sdx format."
-read drive
-clear
-
-#partition drive
-echo "Do you want to manually partition $drive? (y/n)"
-read ans1
-     if  [  "$ans1" = "y"  ]
-          then cfdisk $drive
-     
-     fi
-     clear
-     
-#select partition
-echo "Below is a list of the partitions on $drive."
-lsblk $drive
-echo "Enter the partition where you want to install Arch."
-echo "Please use /dev/sda1 format" 
-read part
-echo "Enter the partition for your swap" 
-read spart
-
-#format and mount partitions 
-mkfs.ext4 $part
-mkswap &spart
-mount $part /mnt
-swapon $spart
-clear
-
-#install base
-echo "Press enter to begin installing Arch onto $part"
-read 
-pacstrap /mnt base base-devel 
-clear
-
-#generate fstab
-echo "Generating fstab..."
-genfstab -U /mnt >> /mnt/etc/fstab
-clear
-
-#chroot into installation
-arch-chroot /mnt /bin/bash
 
 #set root password and create user
 echo "Please enter the root password for your new system."
